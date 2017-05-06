cb145ce355c818e3eb91eb591561e57df8e4374c
Marijn Ophorst <marijn@monomelodies.nl>
2017-05-06 09:32:45 +0200

    entry point

---
+++ src/index.js
@@ -0,0 +1,24 @@
+
+"use strict";
+
+import Service from './Service';
+import './template';
+
+export default angular.module('monad.navigation', ['monad.cms', 'monad.navigation.templates'])
+    .service('monadNavigationService', Service)
+    // Backwards compat with 2.x:
+    .service('moNavigation', Service)
+    .component('monadNavigation', {
+        templateUrl: 'Monad/Navigation/template.html',
+        controller: ['monadNavigationService', function (Service) {
+            this.$onInit = () => {
+                this.menu = this.menu || 'main';
+                this.options = Service.getPath(this.menu);
+            };
+            this.select = item => Service.select(item);
+        }],
+        bindings: {path: '@'}
+    })
+    .name
+    ;
+
