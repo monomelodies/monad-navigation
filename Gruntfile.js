1f302f360ff69e0fac3d827f6e58bf4de763be02
Marijn Ophorst <marijn@monomelodies.nl>
2017-05-07 17:07:32 +0200

    cleanup gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,21 +2,7 @@
 module.exports = function (grunt) {
 
     var pkg = grunt.file.readJSON('package.json');
-    grunt.initConfig({pkg: pkg});
-
-    grunt.loadNpmTasks('grunt-angular-gettext');
-    grunt.config('nggettext_extract', {
-        pot: {
-            files: {
-                'Locale/template.pot': ['src/**/*.{js,html}', 'index.html']
-            }
-        },
-    });
-    grunt.config('nggettext_compile', {
-        all: {
-            files: {'i18n.js': ['Locale/*.po']}
-        }
-    });
+    grunt.initConfig({pkg});
 
     grunt.loadNpmTasks('grunt-angular-templates');
     grunt.config('ngtemplates', {
@@ -37,11 +23,10 @@ module.exports = function (grunt) {
             },
             cwd: 'src',
             src: ['**/*.html', '!index.html'],
-            dest: 'es5/template.js'
+            dest: 'lib/templates.js'
         }
     });
 
-    /*
     grunt.loadNpmTasks('grunt-sass');
     grunt.config('sass', {
         monad: {
@@ -50,45 +35,15 @@ module.exports = function (grunt) {
                 compass: true,
                 sourcemap: 'none'
             },
-            files: {'dist/admin.css': 'src/_sass/default.scss'}
-        }
-    });
-    */
-
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.config('watch', {
-        gettext_extract: {
-            files: ['src/**/*.{js,html}', 'index.html'],
-            tasks: ['nggettext_extract']
-        },
-        gettext_compile: {
-            files: ['Locale/*.po'],
-            tasks: ['nggettext_compile']
-        },
-        spritesheet: {
-            files: ['assets/i18n/**/*.png'],
-            tasks: ['spritesheet']
-        },
-        sass: {
-            files: ['src/_sass/**/*.scss'],
-            tasks: ['sass']
-        },
-        templates: {
-            files: ['src/**/*.html'],
-            tasks: ['ngtemplates']
-        },
-        includereplace: {
-            files: ['src/index.html'],
-            tasks: ['includereplace']
+            files: {'dist/monad-navigation.css': 'src/style.scss'}
         }
     });
 
-    /*
     grunt.loadNpmTasks('grunt-browserify');
     grunt.config('browserify', {
         monad: {
             src: 'src/index.js',
-            dest: 'dist/monad.js',
+            dest: 'dist/monad-navigation.js',
             options: {
                 transform: ['babelify'],
                 standalone: 'monad',
@@ -96,11 +51,40 @@ module.exports = function (grunt) {
             }
         }
     });
-    */
+
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.config('shell', {
+        lib: { command: 'npm run build' },
+        clean: { command: 'rm -rf dist/* && rm -rf lib/*' }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.config('uglify', {
+        js: {
+            src: 'dist/monad-navigation.js',
+            dest: 'dist/monad-navigation.min.js'
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.config('watch', {
+        lib: {
+            files: ['src/**/*.js'],
+            tasks: ['shell:lib']
+        },
+        sass: {
+            files: ['src/style.scss'],
+            tasks: ['sass']
+        },
+        templates: {
+            files: ['src/**/*.html'],
+            tasks: ['ngtemplates']
+        }
+    });
 
     grunt.registerTask('default', ['build']);
-    grunt.registerTask('build', ['ngtemplates', 'gettext', /*'sass',*/ 'includereplace', 'copy'/*, 'browserify'*/]);
-    grunt.registerTask('gettext', ['nggettext_extract', 'nggettext_compile']);
+    grunt.registerTask('build', ['sass', 'shell:lib', 'ngtemplates', 'browserify']);
     grunt.registerTask('dev', ['build', 'watch']);
+    grunt.registerTask('prod', ['shell:clean', 'build', 'uglify']);
 };
 
