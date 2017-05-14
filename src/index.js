eea8530691a4151d6c1ed63bff9b9b5aa2ad8b02
Marijn Ophorst <marijn@monomelodies.nl>
2017-05-14 13:02:22 +0200

    you know what? screw backwards compat. also add main prop and rename path to more logical menu

--- src/index.js
+++ src/index.js
@@ -5,20 +5,17 @@ import Service from './Service';
 import '../lib/template';
 
 export default angular.module('monad.navigation', ['monad.cms', 'monad.navigation.templates'])
-    .service('monadNavigationService', Service)
-    // Backwards compat with 2.x:
-    .service('moNavigation', Service)
+    .service('monadNavigation', Service)
     .component('monadNavigation', {
         templateUrl: 'Monad/Navigation/template.html',
-        controller: ['monadNavigationService', 'monadLocation', function (Service, monadLocation) {
+        controller: ['monadNavigation', 'monadLocation', function (Service, monadLocation) {
             this.$onInit = () => {
-                this.path = this.path || 'main';
-                this.options = Service.getPath(this.path);
+                this.menu = Service.get(this.menu);
             };
             this.select = item => Service.select(item);
             this.url = url => monadLocation.make(url);
         }],
-        bindings: {path: '@'}
+        bindings: {menu: '@', main: '@'}
     })
     .name
     ;
